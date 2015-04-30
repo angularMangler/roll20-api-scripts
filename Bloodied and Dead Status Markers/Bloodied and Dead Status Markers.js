0a8d3b1f06a8b331ade4eace3a11ff83a9f52cc0
angularMangler <eric.hooshmand@aciworldwide.com>
2015-04-30 16:46:24 -0700

    A shorter implementation
    If you care to shorten it, here it is.

--- Bloodied and Dead Status Markers/Bloodied and Dead Status Markers.js
+++ Bloodied and Dead Status Markers/Bloodied and Dead Status Markers.js
@@ -1,25 +1,5 @@
 on("change:graphic", function(obj) {
     if(obj.get("bar1_max") === "") return;
-   
-    if(obj.get("bar1_value") <= obj.get("bar1_max") / 2) {
-        obj.set({
-              status_redmarker: true
-        });
-    }
-    else{
-        obj.set({
-            status_redmarker: false
-        })
-    }
-
-    if(obj.get("bar1_value") <= 0) {
-      obj.set({
-         status_dead: true
-      });
-    }
-    else {
-      obj.set({
-        status_dead: false
-      });
-    }
+    obj.set({ status_redmarker: obj.get("bar1_value") <= obj.get("bar1_max") / 2 });
+    obj.set({status_dead: obj.get("bar1_value") <= 0 });
 });
